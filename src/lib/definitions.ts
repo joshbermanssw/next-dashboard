c1d01198e7154a6711291b5e1d7b876b406d30cd
Josh Berman <joshberman@ssw.com.au>
2026-04-08 18:24:22 +0200

    fix def

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,7 +1,7 @@
 import * as z from "zod"
 
 export const LoginFormSchema = z.object({
-  rememberMe: z.coerce.boolean().optional(),
+  rememberMe: z.preprocess((val) => val === "on" || val === true, z.boolean()).optional(),
   email: z.email({ error: "Please enter a valid email." }).trim(),
   password: z
     .string()
