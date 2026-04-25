9cd2167def581ed4821564f2cc0b37999d57c119
Josh Berman <joshberman@ssw.com.au>
2026-04-25 18:25:30 +0200

    sidebar colour + using floating style

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -21,7 +21,7 @@ export default async function DashboardLayout({
           } as React.CSSProperties
         }
       >
-        <AppSidebar variant="inset" />
+        <AppSidebar variant="floating" collapsible="icon" />
         <SidebarInset>
           <SiteHeader />
           <div className="flex flex-1 flex-col">
